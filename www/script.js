bb982a49fdf75148c7bb5231a0a0aa634fdb0f08
wopox1337 <wopox1337@ya.ru>
2026-01-14 12:20:35 +0300

    fix(js): update JavaScript for Bootstrap 5 compatibility
    - Replace jQuery popover plugin with native Bootstrap 5 Popover API
    - Implement proper popover lifecycle management (dispose and reinitialize)
    - Update Bloodhound search configuration with wildcard parameter
    - Add constants search with direct navigation to include files
    - Fix nav-functions toggle and typeahead search integration
    - Remove debug console logging from typeahead:selected handler
    
    BREAKING CHANGE: Popovers now use bootstrap.Popover() instead of $.fn.popover()

--- www/script.js
+++ www/script.js
@@ -1,42 +1,56 @@
-(function()
-{
-	$( '.function' ).popover(
-	{
-		container: 'body',
-		placement: 'right',
-		trigger: 'hover'
-	} );
-	
-	$( document )
-		.pjax( 'a', '#pjax-container' )
-		.on( 'pjax:start', function() { NProgress.start(); } )
-		.on( 'pjax:end',   function() { NProgress.done();  } )
-		.on( 'pjax:clicked', function( ev )
-			{
-				$( '.function.active' ).removeClass( 'active' );
-				$( ev.target ).parent().addClass( 'active' );
-			} );
-	
-	$( '.file > a' ).on( 'click', function()
-	{
-		var nav = $( '#file-' + $( this ).text() );
-		
-		var visibleNav = $( '.nav-functions.show' );
-		
-		if( !visibleNav.is( nav ) )
-		{
-			visibleNav.removeClass( 'show' );
+(function () {
+	// Initialize Bootstrap 5 popovers
+	function initializePopovers() {
+		var popoverTriggerList = [].slice.call(document.querySelectorAll('.function'));
+		popoverTriggerList.forEach(function (popoverTriggerEl) {
+			// Dispose existing popover if it exists
+			var existingPopover = bootstrap.Popover.getInstance(popoverTriggerEl);
+			if (existingPopover) {
+				existingPopover.dispose();
+			}
+
+			// Create new popover with Bootstrap 5 API
+			new bootstrap.Popover(popoverTriggerEl, {
+				container: 'body',
+				placement: 'right',
+				trigger: 'hover',
+				html: true,
+				title: $(popoverTriggerEl).data('title'),
+				content: $(popoverTriggerEl).data('content')
+			});
+		});
+	}
+
+	// Initial popover initialization
+	initializePopovers();
+
+	$(document)
+		.pjax('a', '#pjax-container')
+		.on('pjax:start', function () { NProgress.start(); })
+		.on('pjax:end', function () { NProgress.done(); initializePopovers(); })
+		.on('pjax:clicked', function (ev) {
+			$('.function.active').removeClass('active');
+			$(ev.target).parent().addClass('active');
+		});
+
+	$('.file > a').on('click', function () {
+		var nav = $('#file-' + $(this).text());
+
+		var visibleNav = $('.nav-functions.show');
+
+		if (!visibleNav.is(nav)) {
+			visibleNav.removeClass('show');
 		}
-		
-		if( nav.hasClass( 'show' ) )
-		{
-			nav.removeClass( 'show' );
+
+		if (nav.hasClass('show')) {
+			nav.removeClass('show');
 		}
-		else
-		{
-			nav.addClass( 'show' );
+		else {
+			nav.addClass('show');
+			// Reinitialize popovers for newly visible functions
+			initializePopovers();
 		}
-	} );
+	});
 
 	// Menu toggle for mobile
 	$('.menu-toggle').on('click', function () {
@@ -49,76 +63,88 @@
 			$('.sidebar').removeClass('open');
 		}
 	});
-	
+
 	var functions = [];
-	
-	$( '.function' ).each( function( )
-	{
-		var $this = $( this );
-		
-		functions.push( [ $this.data( 'title' ), $this.data( 'content' ) ] );
-	} );
-	
-	var constantSearch = new Bloodhound( {
-		datumTokenizer: Bloodhound.tokenizers.obj.whitespace( 'value' ),
+
+	$('.function').each(function () {
+		var $this = $(this);
+
+		functions.push([$this.data('title'), $this.data('content')]);
+	});
+
+	var constantSearch = new Bloodhound({
+		datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
 		queryTokenizer: Bloodhound.tokenizers.whitespace,
-		remote: $( 'body' ).data( 'baseurl' ) + '__search/%QUERY'
-	} );
-	
+		remote: {
+			url: $('body').data('baseurl') + '__search/%QUERY',
+			wildcard: '%QUERY'
+		}
+	});
+
 	constantSearch.initialize();
-	
-	$( '.typeahead' ).typeahead(
-	{
-		hint: true,
-		highlight: true,
-		minLength: 1
-	},
-	{
-		name: 'functions',
-		displayKey: 'value',
-		templates:
+
+	$('.typeahead').typeahead(
 		{
-			header: '<h3 class="tt-name">Functions</h3>'
+			hint: true,
+			highlight: true,
+			minLength: 1
 		},
-		source: function( query, callback )
 		{
-			var matches = [], substrRegex = new RegExp( query, 'i' );
-			
-			$.each( functions, function( i, str )
+			name: 'functions',
+			displayKey: 'value',
+			templates:
 			{
-				if( substrRegex.test( str ) )
-				{
-					matches.push( { value: str[ 0 ] } );
-				}
-			} );
-			
-			callback( matches );
-		}
-	},
-	{
-		name: 'constants',
-		displayKey: 'value',
-		templates:
-		{
-			header: '<h3 class="tt-name">Constants</h3>'
+				header: '<h3 class="tt-name">Functions</h3>'
+			},
+			source: function (query, callback) {
+				var matches = [], substrRegex = new RegExp(query, 'i');
+
+				$.each(functions, function (i, str) {
+					if (substrRegex.test(str)) {
+						matches.push({ value: str[0] });
+					}
+				});
+
+				callback(matches);
+			}
 		},
-		source: constantSearch.ttAdapter()
-	} ).on( 'typeahead:selected', function( a, b, source )
-	{
-		if( source === 'constants' )
 		{
-			$( 'a[data-file="' + b.includeName + '"]' ).click();
-			
-			return;
-		}
-		
-		var func = $( '[data-title="' + b.value + '"]' );
-		
-		$( '.nav-sidebar.show' ).removeClass( 'show' );
-		
-		func
-			.parent().addClass( 'show' )
-			.end()
-			.find( 'a' ).click();
-	} );
+			name: 'constants',
+			displayKey: 'value',
+			templates:
+			{
+				header: '<h3 class="tt-name">Constants</h3>'
+			},
+			source: function (query, syncResults, asyncResults) {
+				if (query === '') {
+					return;
+				}
+
+				constantSearch.search(query, function (results) {
+					asyncResults(results);
+				}, function (results) {
+					asyncResults(results);
+				});
+			}
+		}).on('typeahead:selected', function (a, b, source) {
+			// Check if this is a constant (has includeName but not from functions source)
+			if (b.includeName) {
+				// Direct navigation to constants page for this file
+				var baseUrl = $('body').data('baseurl');
+				var url = baseUrl + b.includeName;
+				window.location.href = url;
+
+				return;
+			}
+
+			// Handle functions selection
+			var func = $('[data-title="' + b.value + '"]');
+
+			$('.nav-functions.show').removeClass('show');
+
+			func
+				.parent().addClass('show')
+				.end()
+				.find('a').click();
+		});
 }());
