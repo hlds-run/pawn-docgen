1f7947ce6d14897c6863a270502e33fd6ddcb30e
wopox1337 <wopox1337@ya.ru>
2026-01-20 02:26:41 +0300

    feat(a11y): improve heading hierarchy and add semantic structure
    - Add <h2> heading for 'How to Use This Documentation' section
    - Add <article> wrapper for main page content
    - Ensure proper heading order: h1 -> h2

--- www/template/main.php
+++ www/template/main.php
@@ -2,12 +2,15 @@
 	require __DIR__ . '/header.php';
 ?>
 
-<h1 class="border-bottom pb-2 mb-3">Welcome to the <?php echo $Project; ?> Scripting API Reference</h1>
+<article>
+	<h1 class="border-bottom pb-2 mb-3">Welcome to the <?php echo $Project; ?> Scripting API Reference</h1>
 
-<p>For more information, see the <a href="http://wiki.alliedmods.net/Category:<?php echo str_replace( ' ', '_', $Project ); ?>_Scripting"><?php echo $Project; ?> Scripting Wiki</a>, which contains tutorials on specific topics.</p>
-<hr>
-<p>Enter a search term on the left to look for symbols in the <?php echo $Project; ?> include files.</p>
-<p>Alternately, click a file on the left to browse its functions/symbols or see its contents.</p>
+	<p>For more information, see the <a href="http://wiki.alliedmods.net/Category:<?php echo str_replace( ' ', '_', $Project ); ?>_Scripting"><?php echo $Project; ?> Scripting Wiki</a>, which contains tutorials on specific topics.</p>
+	<hr>
+	<h2>How to Use This Documentation</h2>
+	<p>Enter a search term on the left to look for symbols in the <?php echo $Project; ?> include files.</p>
+	<p>Alternately, click a file on the left to browse its functions/symbols or see its contents.</p>
+</article>
 
 <?php
 	require __DIR__ . '/footer.php';
